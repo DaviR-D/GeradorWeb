eaf76f1996f5c7c945a1f0c06c8dbc518ead59e2
Davi-RD <davirdavanzo@gmail.com>
2022-03-12 10:41:10 +0000

    Protótipo do template 1

---
+++ frontend/src/components/Template1.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <img :src="image" style="height: 400px; width: 600px" />
+
+    <li v-for="alternative in alternatives" :key="alternative">
+      <button @click="checkAnswer(alternative)">{{ alternative }}</button>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QuestionTemplate1",
+  props: {
+    image: {
+      type: String,
+      required: true,
+    },
+    alternatives: {
+      type: Array,
+
+      required: true,
+    },
+
+    rightAnswer: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    checkAnswer(alternative) {
+      alternative == this.rightAnswer
+        ? console.log("Você acertou!")
+        : console.log("Você errou!");
+    },
+  },
+};
+</script>
