a0c9858c26b4d7a1ad24075c329aaf15825554e9
Davi-RD <davirdavanzo@gmail.com>
2022-04-12 19:34:08 +0000

    Adicionados templates 2 e 6

---
+++ frontend/src/components/Templates/Template2.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <img :src="data.image" style="height: 400px; width: 600px" />
+    <h1>{{ data.text }}</h1>
+    <button @click="nextQuestion()">Entendido</button>
+  </div>
+</template>
+
+<script>
+import { useIndexStore } from "@/stores/index";
+
+export default {
+  name: "TheTemplate2",
+  data() {
+    return {
+      index: useIndexStore(),
+    };
+  },
+
+  props: {
+    data: {
+      type: Object,
+      default: () => ({
+        description: "",
+        image: "",
+        text: "",
+      }),
+    },
+  },
+  methods: {
+    nextQuestion() {
+      this.index.increment();
+    },
+  },
+};
+</script>
