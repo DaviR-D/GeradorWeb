cb633914a1f2e8eab1b0d68870ad0df9b67898df
Davi-RD <davirdavanzo@gmail.com>
2022-04-22 09:39:12 +0000

    Adicionada base do template 3

---
+++ frontend/src/components/Templates/Template3.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h1 class="title">Explicação:</h1>
+    <li v-for="(image, index) in data.images" :key="index">
+      <img :src="image" />
+      <h1>
+        <strong>{{ data.words[index] }}</strong>
+      </h1>
+    </li>
+
+    <h1 class="h1Template2">{{ data.text }}</h1>
+    <button class="btnTemplate2" @click="nextQuestion()">Entendido</button>
+  </div>
+</template>
+
+<script>
+import { useIndexStore } from "@/stores/index";
+
+export default {
+  name: "TheTemplate3",
+  data() {
+    return {
+      index: useIndexStore(),
+    };
+  },
+
+  props: {
+    data: {
+      type: Object,
+      default: () => ({
+        description: "",
+        images: "",
+        words: "",
+      }),
+    },
+  },
+  methods: {
+    nextQuestion() {
+      this.index.increment();
+    },
+  },
+};
+</script>
+<style scoped>
+img {
+  max-width: 40%;
+  max-height: 40%;
+  border-radius: 5px;
+}
+.h1Template2 {
+  text-align: center;
+  background-color: gray;
+  color: white;
+  font-weight: bold;
+  margin: 0 20% 2% 20%;
+  border-radius: 5px;
+}
+.btnTemplate2 {
+  border-radius: 5px;
+  cursor: pointer;
+}
+.title {
+  color: white;
+  text-decoration: underline;
+  text-align: center;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -7,6 +7,7 @@
 <script setup>
 import TheTemplate1 from "@/components/Templates/Template1.vue";
 import TheTemplate2 from "@/components/Templates/Template2.vue";
+import TheTemplate3 from "@/components/Templates/Template3.vue";
 import TheTemplate4 from "@/components/Templates/Template4.vue";
 import TheTemplate5 from "@/components/Templates/Template5.vue";
 import TheTemplate6 from "@/components/Templates/Template6.vue";
@@ -21,6 +22,16 @@ import router from "../router";
 const useIndex = useIndexStore();
 
 const atividade = [
+  {
+    template: TheTemplate3,
+    images: [
+      "https://hddesktopwallpapers.in/wp-content/uploads/2015/09/beagle-dog-breeds-680x425.jpg",
+      "https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/v1555927033/shape/mentalfloss/200265153-001.jpg?itok=pt3-Ofki",
+      "https://www.advantagepetcare.com.au/sites/g/files/adhwdz311/files/styles/paragraph_image/public/2020-07/istock-539027929_unrestricted_1110x800.jpg?itok=jiLQgT-c",
+      "https://fei-fan-production.s3.amazonaws.com/s3fs-public/160920-irish-2.jpg",
+    ],
+    words: ["Dog", "Pig", "Cat", "Horse"],
+  },
   {
     template: TheTemplate1,
     alternatives: ["Cow", "Cat", "Pig", "Bird"],
