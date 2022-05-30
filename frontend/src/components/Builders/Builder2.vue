67f060d42b9646b71a373533e106588c327800d2
Davi-RD <davirdavanzo@gmail.com>
2022-05-30 13:50:35 +0000

    Base do builder 2

---
+++ frontend/src/components/Builders/Builder2.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <input type="file" @change="onFileChange" />
+
+    <input type="text" placeholder="Palavra" v-model="text" />
+
+    <button @click="save()">Salvar</button>
+  </div>
+</template>
+
+<script>
+import { useIndexStore } from "@/stores/index";
+
+export default {
+  name: "TheBuilder2",
+  data() {
+    return {
+      index: useIndexStore(),
+      text: "",
+      image: "",
+      question: {},
+    };
+  },
+
+  props: {},
+  methods: {
+    onFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      this.createImage(files[0]);
+    },
+    createImage(file) {
+      let reader = new FileReader();
+      reader.onload = (e) => {
+        this.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    save() {
+      let question = {
+        template: "TheTemplate2",
+        text: this.text,
+        image: this.image,
+      };
+      this.question = question;
+      //this.index.increment();
+    },
+  },
+};
+</script>
