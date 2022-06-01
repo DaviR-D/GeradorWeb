7c605f6fcc30f8891744853e3f5546ec12f2886f
Davi-RD <davirdavanzo@gmail.com>
2022-06-01 16:27:53 +0000

    Importação dos builders no BuildView

--- frontend/src/views/BuildView.vue
+++ frontend/src/views/BuildView.vue
@@ -6,6 +6,11 @@
 
 <script setup>
 import TheBuilder1 from "@/components/Builders/Builder1.vue";
+import TheBuilder2 from "@/components/Builders/Builder2.vue";
+import TheBuilder3 from "@/components/Builders/Builder3.vue";
+import TheBuilder4 from "@/components/Builders/Builder4.vue";
+import TheBuilder5 from "@/components/Builders/Builder5.vue";
+import TheBuilder6 from "@/components/Builders/Builder6.vue";
 //import { useIndexStore } from "@/stores/index";
 //import router from "../router";
 
