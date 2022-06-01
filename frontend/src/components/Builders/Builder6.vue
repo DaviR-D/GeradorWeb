84607b00271bd262315cf89bc3b0d51cf60b2aa2
Davi-RD <davirdavanzo@gmail.com>
2022-06-01 16:27:07 +0000

    Base do builder 6

---
+++ frontend/src/components/Builders/Builder6.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <input type="file" @change="onFileChange" />
+    <input type="text" placeholder="Palavra" v-model="rightAnswer" />
+    <input type="text" v-model="value" placeholder="Valor da questão" />
+    <button @click="save()">Salvar</button>
+  </div>
+</template>
+
+<script>
+import { useIndexStore } from "@/stores/index";
+
+export default {
+  name: "TheBuilder6",
+  data() {
+    return {
+      index: useIndexStore(),
+      image: "",
+      rightAnswer: "",
+      value: "",
+      question: {},
+    };
+  },
+
+  props: {},
+  methods: {
+    onFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      this.createImage(files[0]);
+    },
+    createImage(file) {
+      let reader = new FileReader();
+      reader.onload = (e) => {
+        this.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    save() {
+      let question = {
+        image: this.image,
+        rightAnswer: this.rightAnswer,
+        value: this.value,
+      };
+      this.question = question;
+      //this.index.increment();
+    },
+  },
+};
+</script>
