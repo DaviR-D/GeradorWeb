d4d894a34542466d0c79d296de946ac0ccf074ff
Davi-RD <davirdavanzo@gmail.com>
2022-06-01 16:03:22 +0000

    Base do builder 3

---
+++ frontend/src/components/Builders/Builder3.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <li v-for="i in [0, 1, 2, 3]" :key="i">
+      <input type="file" @change="(e) => onFileChange(e, i)" />
+      <input type="text" :placeholder="i + 1" v-model="words[i]" />
+    </li>
+
+    <button @click="save()">Salvar</button>
+  </div>
+</template>
+
+<script>
+import { useIndexStore } from "@/stores/index";
+
+export default {
+  name: "TheBuilder3",
+  data() {
+    return {
+      index: useIndexStore(),
+      words: [],
+      images: [],
+      question: {},
+    };
+  },
+
+  props: {},
+  methods: {
+    onFileChange(e, i) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      this.createImage(files[0], i);
+    },
+    createImage(file, i) {
+      let reader = new FileReader();
+      reader.onload = (e) => {
+        this.images[i] = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    save() {
+      let question = {
+        words: this.words,
+        images: this.images,
+      };
+      this.question = question;
+      //this.index.increment();
+    },
+  },
+};
+</script>
