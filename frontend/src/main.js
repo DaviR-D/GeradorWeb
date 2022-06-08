2a6c25ec407b401bdab964a9a0949291561a7db1
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-06-07 23:05:31 -0300

    CSS templates

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,16 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 import VueAxios from "vue-axios";
+
+
+
+
 import PrimeVue from "primevue/config";
 import "primevue/resources/themes/saga-blue/theme.css"; //theme
 import "primevue/resources/primevue.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
+
+import Button from "primevue/button";
 
 const app = createApp(App);
 
@@ -15,5 +21,7 @@ app.use(createPinia());
 app.use(router);
 app.use(VueAxios, axios);
 app.use(PrimeVue);
+
+app.component('Button',Button);
 
 app.mount("#app");
