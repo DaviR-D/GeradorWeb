c9337d6b01573a295926f055eb783336d4d9b79f
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-06-07 23:04:53 -0300

    CSS templates

--- frontend/src/components/Templates/Template2.vue
+++ frontend/src/components/Templates/Template2.vue
@@ -3,7 +3,7 @@
     <h1 class="title">Explicação:</h1>
     <img :src="data.image" />
     <h1 class="h1Template2">{{ data.text }}</h1>
-    <button class="btnTemplate2" @click="nextQuestion()">Entendido</button>
+    <Button class="primary-button mr-2 mb-2" @click="nextQuestion()">Entendido</Button>
   </div>
 </template>
 
@@ -50,9 +50,12 @@ img {
   margin: 0 20% 2% 20%;
   border-radius: 5px;
 }
-.btnTemplate2 {
-  border-radius: 5px;
-  cursor: pointer;
+.primary-button{
+  margin-bottom: 1%;
+  justify-content: center;
+  height: 60px;
+  font-weight: bold;  
+  font-size: large;
 }
 .title {
   color: white;
