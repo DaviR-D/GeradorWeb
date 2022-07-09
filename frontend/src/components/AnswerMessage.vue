12addb8bd04ef244ebcb534a749cef2694a42cc9
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-07-09 16:28:31 -0300

    Arrumando css AnswerMessage

--- frontend/src/components/AnswerMessage.vue
+++ frontend/src/components/AnswerMessage.vue
@@ -1,6 +1,10 @@
 <template>
-  <div :style="messageColor">
-    <h1>{{ answerMessage }} <button @click="index.increment()">OK</button></h1>
+  <div :style="messageColor" class="container">
+    <h1 class="h1Message">{{ answerMessage }}</h1>
+    <h1 class="h1Button">
+      <img class="acertou" :src="imageURL" alt="">
+      <button style="border-radius: 5px" @click="index.increment()">OK</button>
+    </h1>
   </div>
 </template>
 
@@ -17,6 +21,7 @@ export default {
       index: useIndexStore(),
       answerMessage: "",
       messageColor: "",
+      imageURL: "",
     };
   },
 
@@ -33,14 +38,36 @@ export default {
       if (this.rightAnswer) {
         this.answerMessage =
           "Parabéns, você acertou! +" + this.value + " pontos";
-        this.messageColor = "background-color: green";
+        this.messageColor = "background-color: #32CD32";
+        this.imageURL = "https://i.imgur.com/XgL6soA.png";
       } else {
-        this.messageColor = "background-color: green";
+        this.messageColor = "background-color: black";
         this.answerMessage = "Você errou";
-        this.messageColor = "background-color: red";
+        this.messageColor = "background-color: #ff261b";
+        this.imageURL = "https://i.imgur.com/nztO8KF.png";
       }
       this.answered = true;
     },
   },
 };
 </script>
+<style scoped>
+.container {}
+
+.acertou {
+  max-width: 10%;
+  padding-right: 1%;
+
+}
+
+.h1Message {
+  justify-content: center;
+  border-radius: 5px;
+}
+
+.h1Button {
+  display: flex;
+  align-items: center;
+  padding-right: 1%;
+}
+</style>
--- frontend/src/views/FinishView.vue
+++ frontend/src/views/FinishView.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
+  <div class="container">
     <h1>
       Parabéns, você finalizou a atividade, acertou {{ answerCount }} de
       {{ questionCount }} questões e teve uma pontuação final de {{ score }}
     </h1>
+    <img src="https://i.imgur.com/XgL6soA.png" alt="">
   </div>
 </template>
 <script setup>
@@ -15,3 +16,8 @@ const score = store.getScore;
 const answerCount = store.getAnswerCount;
 const questionCount = store.getQuestionCount;
 </script>
+<style>
+.container{
+  justify-content: center;
+}
+</style>
