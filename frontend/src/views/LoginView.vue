db82b4cae06489b594db0f20bb985246d5aaa50f
felipeferreiradev <felipeferreira.sp.dev@gmail.com>
2022-07-18 16:29:55 -0300

    Tela de login

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
-    <span class="p-float-label">
-      <InputText id="username" type="text" v-model="username" />
-      <label for="username">Username</label>
-    </span>
-    <Password v-model="password" :feedback="false" />
+  <div class="container">
+    <i
+      class="pi pi-users"
+      style="font-size: 5rem; color: whitesmoke; margin-bottom: 50px"
+    ></i>
+    <InputText
+      id="username"
+      type="text"
+      v-model="username"
+      placeholder="E-mail"
+    />
+    <Password
+      v-model="password"
+      :feedback="false"
+      class="inputPassword"
+      placeholder="Password"
+    />
+    <button @click="Login">Login</button>
   </div>
-  <button @click="Login"></button>
 </template>
 
 <script>
@@ -43,3 +54,35 @@ export default {
   },
 };
 </script>
+
+<style>
+.container {
+  border: solid 1px #29292e;
+  padding: 10px;
+  border-radius: 4px;
+  width: 450px;
+  height: 500px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.inputPassword {
+  margin: 10px 0 10px;
+}
+
+button {
+  border-radius: 4px !important;
+  color: whitesmoke;
+  font-weight: bold;
+  font-size: 24px;
+  cursor: pointer;
+  transition: ease 0.75s;
+}
+
+button:hover {
+  opacity: 0.78;
+}
+</style>
