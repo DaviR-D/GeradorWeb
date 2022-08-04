770dc78f9eedf2664a6a246d09e7a2c7a23ec96d
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-08-04 06:54:47 -0300

    test

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,13 +5,10 @@ import router from "./router";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
-import PrimeVue from "primevue/config";
-
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(VueAxios, axios);
-app.use(PrimeVue);
 
 app.mount("#app");
