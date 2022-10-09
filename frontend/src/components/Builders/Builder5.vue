52977fbbe34d69d8f19a37a8d55d994dee3764a8
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-10-09 15:09:56 -0300

    Update Builder5.vue

--- frontend/src/components/Builders/Builder5.vue
+++ frontend/src/components/Builders/Builder5.vue
@@ -1,18 +1,37 @@
 <template>
-  <div>
-    <input type="text" placeholder="Palavra" v-model="word" />
-    <li v-for="i in [0, 1, 2, 3]" :key="i">
-      {{ i }}
-      <input type="file" @change="(e) => onFileChange(e, i)" />
-    </li>
+  <div class="h-screen w-screen flex items-center justify-center text-white">
+    <div class="border-[1px] flex flex-col p-4 gap-4 items-center rounded">
+      <h1>Imagem de exemplo:</h1>
+      <img src="https://i.imgur.com/8qSh1bg.png" class="w-full" alt="">
+      <input
+        class="rounded p-2 shadow-sm w-96 shadow-slate-50"
+        type="text"
+        placeholder="Palavra"
+        v-model="word"
+      />
+      <li class="list-none" v-for="i in [0, 1, 2, 3]" :key="i">
+        {{ i }}
+        <input type="file" @change="(e) => onFileChange(e, i)" />
+      </li>
 
-    <input type="text" v-model="value" placeholder="Valor da questão" />
-    <input
-      type="text"
-      v-model="rightAnswer"
-      placeholder="Alternativa correta (número)"
-    />
-    <button @click="save()">Salvar</button>
+      <input
+        class="rounded p-2 shadow-sm w-96 shadow-slate-50"
+        type="text"
+        v-model="value"
+        placeholder="Valor da questão"
+      />
+      <input
+        class="rounded p-2 shadow-sm w-96 shadow-slate-50"
+        type="text"
+        v-model="rightAnswer"
+        placeholder="Alternativa correta (número)"
+      />
+      <a
+        class="anchor text-xl flex items-center justify-center h-12"
+        @click="save()"
+        >Salvar</a
+      >
+    </div>
   </div>
 </template>
 
