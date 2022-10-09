65d0d6f1fd11374ea4fdf74b38df735e66413ea9
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-10-09 15:08:00 -0300

    Update Builder2.vue

--- frontend/src/components/Builders/Builder2.vue
+++ frontend/src/components/Builders/Builder2.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    <input type="file" @change="onFileChange" />
+  <div class="h-screen w-screen flex items-center justify-center text-white">
+    <div class="border-[1px] flex flex-col p-4 gap-4 items-center rounded">
+      <h1>Imagem de exemplo:</h1>
+      <img src="https://i.imgur.com/Evzg5OW.png" class="w-full" alt="" />
+      <input
+        class="rounded p-2 w-96 text-lg"
+        type="file"
+        @change="onFileChange"
+      />
+      <input
+        class="rounded p-2 shadow-sm w-96 shadow-slate-50"
+        type="text"
+        placeholder="Digite a palavra correta"
+        v-model="text"
+      />
 
-    <input type="text" placeholder="Digite a palavra correta" v-model="text" />
-
-    <button @click="save()">Salvar</button>
+      <a
+        class="anchor text-xl flex items-center justify-center h-12"
+        @click="save()"
+        >Salvar</a
+      >
+    </div>
   </div>
 </template>
 
