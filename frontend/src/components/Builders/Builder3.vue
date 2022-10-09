75010668e36471b707c57d9447bff6b8430bf3f8
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-10-09 15:08:10 -0300

    Update Builder3.vue

--- frontend/src/components/Builders/Builder3.vue
+++ frontend/src/components/Builders/Builder3.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-    <ol>
-      <li v-for="i in [0, 1, 2, 3]" :key="i">
-        <input type="file" @change="(e) => onFileChange(e, i)" />
-        <input type="text" :placeholder="i + 1" v-model="words[i]" />
-      </li>
-    </ol>
-
-    <button @click="save()">Salvar</button>
+  <div class="h-screen w-screen flex items-center justify-center text-white">
+    <div class="border-[1px] flex flex-col p-4 gap-4 items-center rounded">
+      <h1>Imagem de exemplo:</h1>
+      <img src="https://i.imgur.com/27qOslm.png" class="w-full" alt="" />
+      <ol>
+        <li v-for="i in [0, 1, 2, 3]" :key="i">
+          <input type="file" @change="(e) => onFileChange(e, i)" />
+          <input type="text" :placeholder="i + 1" v-model="words[i]" />
+        </li>
+      </ol>
+      <a
+        class="anchor text-xl flex items-center justify-center h-12"
+        @click="save()"
+        >Salvar</a
+      >
+    </div>
   </div>
 </template>
 
