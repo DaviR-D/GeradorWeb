00625510fe257a6f746d69eb3f8935f019f5d761
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-10-09 14:02:49 -0300

    Update FinishView.vue

--- frontend/src/views/FinishView.vue
+++ frontend/src/views/FinishView.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-    <img src="../assets/100daysui_100icon.png" alt="Troféu" />
-    <h1>Parabéns</h1>
-    <p>
-      Você finalizou a atividade, acertou {{ answerCount }} de
-      {{ questionCount }} questões e teve uma pontuação final de {{ score }}
+  <div
+    class="flex flex-col h-screen items-center justify-center text-white font-bold"
+  >
+    <img src="../assets/100daysui_100icon.png" class="w-56 h-40" alt="Troféu" />
+    <h1 class="text-5xl">Parabéns</h1>
+    <p class="text-2xl mb-8">
+      Você finalizou a atividade, acertou
+      <span class="text-3xl text-green-400">{{ answerCount }}</span> de
+      <span class="text-3xl text-green-400">{{ questionCount }}</span> questões
+      e teve uma pontuação final de
+      <span class="text-3xl text-green-400">{{ score }}</span>
     </p>
 
-    <a href="/">Voltar</a>
+    <a class="anchor text-xl flex items-center justify-center h-12" href="/"
+      >Voltar</a
+    >
   </div>
 </template>
 <script setup>
