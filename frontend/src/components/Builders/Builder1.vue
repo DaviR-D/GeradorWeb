7db4b59da9cb9511539b8adae34baa5111e638c9
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2022-10-09 15:07:38 -0300

    css builder 1

--- frontend/src/components/Builders/Builder1.vue
+++ frontend/src/components/Builders/Builder1.vue
@@ -1,14 +1,37 @@
 <template>
-  <div>
-    <input type="file" @change="onFileChange" />
-    <input
-      type="text"
-      placeholder="Escreva as opções separando às por vírgulas"
-    />
-    <p>Exemplo: Carro, Moto, Casa, Computador</p>
-    <input type="text" v-model="rightAnswer" placeholder="Resposta correta" />
-    <input type="text" v-model="value" placeholder="Valor da questão" />
-    <a class="" @click="save()">Salvar</a>
+  <div class="h-screen w-screen flex items-center justify-center text-white">
+    <div class="border-[1px] flex flex-col p-4 gap-4 items-center rounded">
+      <h1>Imagem de exemplo:</h1>
+      <img src="https://i.imgur.com/fSBLcSb.png" class="w-full" alt="" />
+      <input
+        class="rounded p-2 w-96 text-lg"
+        type="file"
+        @change="onFileChange"
+      />
+      <input
+        class="rounded p-2 shadow-sm w-96 shadow-slate-50"
+        type="text"
+        placeholder="Escreva as opções separando às por vírgulas"
+      />
+      <p class="text-lg mb-4">Exemplo: Carro, Moto, Casa, Computador</p>
+      <input
+        class="rounded p-2 shadow-sm w-96 shadow-slate-50"
+        type="text"
+        v-model="rightAnswer"
+        placeholder="Resposta correta"
+      />
+      <input
+        class="rounded p-2 shadow-sm w-96 shadow-slate-50"
+        type="text"
+        v-model="value"
+        placeholder="Valor da questão"
+      />
+      <a
+        class="anchor text-xl flex items-center justify-center h-12"
+        @click="save()"
+        >Salvar</a
+      >
+    </div>
   </div>
 </template>
 
