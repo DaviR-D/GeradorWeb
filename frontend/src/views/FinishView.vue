d362b7f720ff7748178fa9245105abc017948004
Davi-RD <davirdavanzo@gmail.com>
2023-06-13 09:37:28 +0000

    Pontuação só é mostrada e salva caso existam questões

--- frontend/src/views/FinishView.vue
+++ frontend/src/views/FinishView.vue
@@ -4,13 +4,15 @@
   >
     <img src="../assets/100daysui_100icon.png" class="w-56 h-40" alt="Troféu" />
     <h1 class="text-5xl">Parabéns</h1>
-    <p class="text-2xl mb-8">
+    <p v-if="questionCount > 0" class="text-2xl mb-8">
       Você finalizou a atividade, acertou
       <span class="text-3xl text-green-400">{{ answerCount }}</span> de
       <span class="text-3xl text-green-400">{{ questionCount }}</span> questões
       e teve uma pontuação final de
       <span class="text-3xl text-green-400">{{ score }}</span>
     </p>
+
+    <p v-else class="text-2xl mb-8">Você finalizou esta aula!</p>
 
     <a class="anchor text-xl flex items-center justify-center h-12" href="/"
       >Voltar</a
@@ -29,17 +31,19 @@ const score = store.getScore;
 const answerCount = store.getAnswerCount;
 const questionCount = store.getQuestionCount;
 
-axios
-  .post(
-    "http://localhost:3000/scores/" + route.params.lessonId,
-    { score: score },
-    {
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("token"),
-      },
-    }
-  )
-  .then((response) => {
-    console.log(response);
-  });
+if (questionCount > 0) {
+  axios
+    .post(
+      "http://localhost:3000/scores/" + route.params.lessonId,
+      { score: score },
+      {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      }
+    )
+    .then((response) => {
+      console.log(response);
+    });
+}
 </script>
