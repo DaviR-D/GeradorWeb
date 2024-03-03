943702073513f63a0ae1cff78bbb2efe05a0a064
Davi-RD <davirdavanzo@gmail.com>
2024-03-02 21:14:32 -0300

    Pequena melhora de design no pop-up de nova atividade

--- frontend/src/components/NewLesson.vue
+++ frontend/src/components/NewLesson.vue
@@ -48,5 +48,6 @@ export default {
   margin: auto;
   background-color: rgb(54, 54, 54);
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 4px;
 }
 </style>
