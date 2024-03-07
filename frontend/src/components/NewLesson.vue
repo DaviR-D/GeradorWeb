93e6fc9ae5533d3d9f1730662f17cf747bfb2cae
Davi-RD <davirdavanzo@gmail.com>
2024-03-07 20:44:52 -0300

    Correção de sobreposição

--- frontend/src/components/NewLesson.vue
+++ frontend/src/components/NewLesson.vue
@@ -49,5 +49,6 @@ export default {
   background-color: rgb(54, 54, 54);
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   border-radius: 4px;
+  z-index: 1;
 }
 </style>
